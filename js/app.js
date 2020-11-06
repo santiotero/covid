1e298293d0aa337ec98ab9bcaebf355782d8a388
exa85897 <santiago.otero@claro.com.ar>
2020-11-06 15:53:31 -0300

    local db + navegacion 1

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-var appUser;
+var user;
+var db;
 
 window.onload = function() {
-
     init();    
-    setServiceWorker();
-
+    setServiceWorker();    
+    setTriggers();
 }
 
 
@@ -25,6 +25,14 @@ function setServiceWorker(){
     }
 
 }
+
+function setTriggers(){
+
+  $("#form_registro .submit").click(function() {
+    createUser();
+  });
+
+}
   
 function optionMenu(num){
 
@@ -39,7 +47,7 @@ function optionMenu(num){
       case 0:
         $('#initloading').show();
         setTimeout(function(){
-          validateUser();  
+          intialvalidation();  
         }, 2000);
         break;
 
@@ -53,14 +61,17 @@ function optionMenu(num){
 
       case 3:
         $('#datos').show();
+        loadUser();
         break;
 
       case 4:
         $('#contactos').show();
+        loadContacts();
         break;
 
       case 5:
         $('#contagiados').show();
+        loadCovidContacts();
         break;
 
       default:        
@@ -69,14 +80,79 @@ function optionMenu(num){
 
 }
 
-function validateUser(){
 
-  if(appUser == {} || appUser == null || !appUser){
-    optionMenu(1);
+
+async function intialvalidation(){
+
+  db = new Dexie('covid_db');
+  db.version(1).stores({users: "phoneNumber,name,lastName,covidDate"});
+  if (!(await Dexie.exists(db.name))) {    
+    optionMenu(1);    
   }else{
-    optionMenu(3);
+     await db.open();        
+     let count = await db.users.count();
+     if(count == 1){
+        user = await db.users.limit(1).first();
+        optionMenu(4);
+        loadUser();
+     }        
+  }
+}
+
+
+function loadUser(){
+  
+  if(user !== undefined){ 
+    $("#form_datos input[name=name]").val(user.name);
+    $("#form_datos input[name=last_name]").val(user.lastName);
+    $("#form_datos input[name=phone]").val(user.phoneNumber);
+    if(user.covidDate != null){
+      $("#form_datos input[name=covid]").prop('checked', true);
+    }
+  }else{
+    optionMenu(1);
   }
 
 }
 
+async function createUser(){
 
+  let covidDate = new Date();  
+  
+  if( $("#form_registro input[name=covid]").prop('checked') ){
+    covidDate = covidDate.toISOString().split('T')[0] + ' ' + covidDate.toTimeString().split(' ')[0];
+  }else{
+    covidDate = null;
+  }
+
+  await db.users.add({
+          phoneNumber: $("#form_registro input[name=phone]").val(),
+          name: $("#form_registro input[name=name]").val(),
+          lastName: $("#form_registro input[name=last_name]").val(),
+          covidDate: covidDate
+  });
+  user = await db.users.limit(1).first();
+
+}
+
+function loadContacts(){
+
+  if(user !== undefined){ 
+    
+  }else{
+    optionMenu(1);
+  }
+
+}
+
+function loadCovidContacts(){
+
+  if(user !== undefined){ 
+    
+  }else{
+    optionMenu(1);
+  }
+
+}
+
+ 
