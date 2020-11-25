5e6e8f6b629cdc1dd6af93ece9986aeed3fe4365
exa85897 <santiago.otero@claro.com.ar>
2020-11-25 13:16:45 -0300

    correccion notificaciones 4

--- js/app.js
+++ js/app.js
@@ -597,10 +597,10 @@ function sendShowNotification(){
   db.infected.where("status").equals(0).toArray().then( function(infecteds){
     if(infecteds !== undefined && infecteds.length>0){
       Push.create("Covid App", {
-           body: "Hay nuevos contagiados en tus contactos",
-           icon: 'img/icons/logo-32.png',
+             body: "Hay nuevos contagiados en tus contactos",
+             icon: 'img/icons/logo-32.png',
           timeout: 60000,          
-          silent: false          
+           silent: false
       }).then( () => {
         db.infected.where("status").equals(0).modify({status: 1});
       });
