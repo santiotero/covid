af288e68e7bec234eb84c620c5bb182424226379
exa85897 <santiago.otero@claro.com.ar>
2020-12-17 13:02:24 -0300

    notificaction push mejorada 9

--- sw.js
+++ sw.js
@@ -82,6 +82,28 @@ self.addEventListener("push", function(event){
 	initSync(updateFriends, fetchUrlPost);
 });
 
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click';  
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({
+      type: "window"
+    })
+    .then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url == '/' && 'focus' in client)
+          return client.focus();
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    })
+  );
+
+});
+
 
 function initSync(updateFriends, fetchUrlPost){
 
